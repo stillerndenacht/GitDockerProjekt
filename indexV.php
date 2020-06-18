5e85c8f8f2e15382a709848250d39182d1273f5e
Veit <vanuit@gmx.de>
2020-06-18 22:04:02 +0200

    gelöscht

--- indexV.php
+++ indexV.php
@@ -1,2 +1,2 @@
 <?php
-echo "Hallo Docker du, ich hab dir einen Container aus dem Kreuz geleiert und er läuft!"; 
+echo "Hallo Docker du, ich hab dir einen Container aus dem Kreuz geleiert und er läuft!"; ?>
